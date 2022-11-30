6409141f2a872362a1355ba2bc4d401a3c7b0bcb
petershom <petershom644@gmail.com>
2022-11-30 18:21:06 +0100

    Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    '''
+    Prints the numbers from 1 to 100 separated by a space
+    but for multiples of 3 prints 'Fizz' and multiple of
+    5 prints 'Buzz'
+    '''
+    for i in range(1, 101):
+        print('{:s}{:s}{:s}'.format(
+            str(i) * ((i % 3 != 0) and (i % 5 != 0)),
+            'Fizz' * (i % 3 == 0),
+            'Buzz' * (i % 5 == 0),
+        ), end=' ')
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
