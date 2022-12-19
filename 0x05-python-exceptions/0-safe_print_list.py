d08affda39e69e11765bfbe830fdcff15a6a8556
petershom <petershom644@gmail.com>
2022-12-19 19:07:26 +0100

    initial commit, checking mandatories

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
     try:
-        for i in range(x):
-            print(my_list=[], end=' ')
+        count = 0
+        while count < x:
+            print("{}".format(my_list[count]), end='')
             count += 1
-    except indexError:
+        print()
+        return count
+    except IndexError:
+        print()
+        return count
         pass
-    print()
-    return count
-
